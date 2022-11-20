a759874beff31d0887381b57c0a1462ecae3266d
yongbeomkwak <yongbeomkwak@gmail.com>
2022-11-20 15:17:53 +0900

    first Init

---
+++ RaniPaper/RaniPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaniPaperApp.swift
+//  RaniPaper
+//
+//  Created by yongbeomkwak on 2022/11/20.
+//
+
+import SwiftUI
+
+@main
+struct RaniPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
