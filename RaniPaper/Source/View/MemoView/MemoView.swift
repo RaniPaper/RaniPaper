0e854ba59f1c690b057c7e1dcac2b00f5e80cd4b
yongbeomkwak <yongbeomkwak@gmail.com>
2022-11-27 23:49:53 +0900

    :lipstick: 더미뷰2를 메모뷰로 교체

---
+++ RaniPaper/Source/View/MemoView/MemoView.swift
@@ -0,0 +1,20 @@
+//
+//  MemoView.swift
+//  RaniPaper
+//
+//  Created by yongbeomkwak on 2022/11/27.
+//
+
+import SwiftUI
+
+struct MemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoView()
+    }
+}
