13a6a22cc45e92e13484f13758e0410bc8e034c0
yongbeomkwak <yongbeomkwak@gmail.com>
2022-11-28 13:54:15 +0900

    [CHORE] 캘린더 뷰 생성

---
+++ RaniPaper/Source/View/CalendarView/CalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarView.swift
+//  RaniPaper
+//
+//  Created by yongbeomkwak on 2022/11/28.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
