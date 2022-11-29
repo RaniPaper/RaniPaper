96bf3ef6182aedffc462f46b5a42e847f1092d5f
yongbeomkwak <yongbeomkwak@gmail.com>
2022-11-29 22:31:25 +0900

    [CHORE] 에딧 뷰 및 키보드 반응 설정

--- RaniPaper/Source/ViewModel/MainViewModel.swift
+++ RaniPaper/Source/ViewModel/MainViewModel.swift
@@ -13,26 +13,13 @@ final class MainViewModel : ObservableObject {
     @Published var isLoading:Bool
     @Published var lockState:LockState
     @Published var selection: ViewSelection
-    @Published private(set) var keyboardHeight: CGFloat = 0
-    private var subscription: AnyCancellable?
-    private let keyboardWillShow =  NotificationCenter.default
-        .publisher(for: UIResponder.keyboardWillShowNotification)
-        .compactMap { output in
-            (output.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect)?.height
-            // 유저 정보 맵에서 keyboard 높이를 얻는다.
-        }
-
-    private let keyboardWillHide = NotificationCenter.default
-        .publisher(for: UIResponder.keyboardWillHideNotification)
-        .map { _ in CGFloat.zero}
+   
 
     init(){
         self.isLoading = true
         self.lockState = .locked
         self.selection = .home
-        subscription = Publishers.Merge(keyboardWillShow, keyboardWillHide)
-                   .subscribe(on: DispatchQueue.main) // UI 변화 이므로 메인. 쓰레드
-                   .assign(to: \.self.keyboardHeight, on: self)
+
         
         print("✅ MainViewModel 생성")
     }
