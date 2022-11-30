66e008c06bb81f459018e939c4d2f7178d9da41b
yongbeomkwak <yongbeomkwak@gmail.com>
2022-11-30 15:57:57 +0900

    [CHORE] 이세돌 펄스널컬러 적용

--- RaniPaper/Source/Model/TaskModel.swift
+++ RaniPaper/Source/Model/TaskModel.swift
@@ -13,7 +13,7 @@ struct TaskModel: Identifiable,Codable{
     var id = UUID().uuidString
     var title:String
     var deadLine: Date = Date()
-    var color:String = "Tree"
+    var color:String
     var ticket:String
 }
 
@@ -40,20 +40,29 @@ func getSampleDate(offset: Int)->Date {
 var tasks: [TaskMetaData] = [
     
     TaskMetaData(task: [
-        TaskModel(title: "Talk1_1 ",ticket: "우왁굳"),
-        TaskModel(title: "Talk1_2 ",ticket: "아이네"),
-        TaskModel(title: "Talk1_3 ",ticket: "라라"),
+        TaskModel(title: "Talk1_1 ",color: "ine",ticket: "우왁굳"),
+        TaskModel(title: "Talk1_2 ",color: "jingburger",ticket: "아이네"),
+        TaskModel(title: "Talk1_3 ",color: "lilpa",ticket: "릴파"),
+        TaskModel(title: "Talk1_3 ",color: "jururu",ticket: "주르르"),
+        TaskModel(title: "Talk1_3 ",color: "gosegu",ticket: "고세구"),
+        TaskModel(title: "Talk1_3 ",color: "viichan",ticket: "비챤"),
     ], taskDate: getSampleDate(offset: 1) ),
     TaskMetaData(task: [
-        TaskModel(title: "Talk2_1 ",ticket: "우왁굳"),
-        TaskModel(title: "Talk2_2 ",ticket: "우왁굳"),
-        TaskModel(title: "Talk2_3 ",ticket: "우왁굳"),
+        TaskModel(title: "Talk1_1 ",color: "ine",ticket: "우왁굳"),
+        TaskModel(title: "Talk1_2 ",color: "jingburger",ticket: "아이네"),
+        TaskModel(title: "Talk1_3 ",color: "lilpa",ticket: "릴파"),
+        TaskModel(title: "Talk1_3 ",color: "jururu",ticket: "주르르"),
+        TaskModel(title: "Talk1_3 ",color: "gosegu",ticket: "고세구"),
+        TaskModel(title: "Talk1_3 ",color: "viichan",ticket: "비챤"),
     ], taskDate: getSampleDate(offset: -3) ),
     
     TaskMetaData(task: [
-        TaskModel(title: "Talk3_1 ",ticket: "우왁굳"),
-        TaskModel(title: "Talk3_2 ",ticket: "우왁굳"),
-        TaskModel(title: "Talk3_3 ",ticket: "우왁굳"),
+        TaskModel(title: "Talk1_1 ",color: "ine",ticket: "우왁굳"),
+        TaskModel(title: "Talk1_2 ",color: "jingburger",ticket: "아이네"),
+        TaskModel(title: "Talk1_3 ",color: "lilpa",ticket: "릴파"),
+        TaskModel(title: "Talk1_3 ",color: "jururu",ticket: "주르르"),
+        TaskModel(title: "Talk1_3 ",color: "gosegu",ticket: "고세구"),
+        TaskModel(title: "Talk1_3 ",color: "viichan",ticket: "비챤"),
     ], taskDate: getSampleDate(offset: -8) )
 ]
 
