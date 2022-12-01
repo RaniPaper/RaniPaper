60d731140cce3dfdcaf9a0a18ff273c3f1436385
youn9k <ace_lephant@naver.com>
2022-12-01 12:39:57 +0900

    :sparkles: 메모 모델 추가

---
+++ RaniPaper/Source/Model/MemoModel.swift
@@ -0,0 +1,15 @@
+//
+//  MemoModel.swift
+//  RaniPaper
+//
+//  Created by YoungK on 2022/12/01.
+//
+
+import Foundation
+
+struct MemoModel: Identifiable, Codable {
+    var id = UUID().uuidString
+    var date: Date = Date()
+    var title:String
+    var content: String
+}
