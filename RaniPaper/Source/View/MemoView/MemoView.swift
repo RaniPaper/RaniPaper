ceb237a7713e4bb69e203bb5b0f7bf7c34d60349
yongbeomkwak <yongbeomkwak@gmail.com>
2022-12-01 22:13:59 +0900

    :lipstick: 메모뷰 스켈레톤 완성

--- RaniPaper/Source/View/MemoView/MemoView.swift
+++ RaniPaper/Source/View/MemoView/MemoView.swift
@@ -8,116 +8,175 @@
 import SwiftUI
 
 struct MemoView: View {
+    @ObservedObject var viewModel:MemoViewModel = MemoViewModel()
+    let columns:[GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+    let memos:[MemoModel] = [MemoModel(title: "제목1", content: "간다라ㅏㅁ암앎ㅇ라asdasdsdadasdaas"),
+                             MemoModel(title: "제목2", content: "내용22222222222222222222"),
+                             MemoModel(title: "제목3", content: "내용33333333333333333333"),
+                             MemoModel(title: "제목4", content: "내용44444444444444444444"),
+                             MemoModel(title: "제목5", content: "내용55555555555555555555"),
+                             MemoModel(title: "제목6", content: "내용66666666666666666666"),
+                             MemoModel(title: "제목7", content: "내용66666666666666666666"),
+                             MemoModel(title: "제목8", content: "내용66666666666666666666"),
+                             MemoModel(title: "제목9", content: "내용66666666666666666666")]
     var body: some View {
-        VStack{
-            
-            GeometryReader { proxy in
-                let thin:CGFloat = 5
-                let thinPadding:CGFloat = 10
-                ZStack {
-                    Color.memoBg
-                    //왼쪽 사각형
-                    VStack{
-                        Rectangle()
-                            .fill(Color.memoPrimary)
-                            .frame(maxWidth:thin,maxHeight: proxy.size.height)
-                            .padding(.leading,thinPadding)
-                    }.frame(maxWidth:proxy.size.width,alignment: .leading)
-                    
-                    VStack(spacing: 5) {
-                    
-                        Text("기록")
-                            .font(.efDiary(30))
-                            .foregroundColor(.memoPrimary)
-                        Text("(-)")
-                            .font(.efDiary(20))
-                            .foregroundColor(.memoPrimary)
-                            .frame(maxWidth:proxy.size.width)
-                            .overlay {
-                                HStack{
-                                    Button {
-                                        
-                                    } label: {
-                                        HStack(spacing: 0){
-                                            Text("크게보기")
-                                                .font(.kotra(20))
-                                                .foregroundColor(.memoPrimary)
-                                            Image(systemName: "arrowtriangle.down.fill").foregroundColor(.memoPrimary)
-                                        }
-                                    }
-                                    .overlay(alignment:.topLeading) {
-                                        
-                                        if true{
-                                            VStack(spacing:2){
-                                                
-                                                Text("크게보기")
+        ZStack {
+            VStack(spacing:0){
+                
+                GeometryReader { proxy in
+                    let thin:CGFloat = 5
+                    let thinPadding:CGFloat = 10
+                    ZStack {
+                        //MARK: 왼쪽 사각형
+                        VStack{
+                            Rectangle()
+                                .fill(Color.memoPrimary)
+                                .frame(maxWidth:thin,maxHeight: proxy.size.height)
+                                .padding(.leading,thinPadding)
+                        }.frame(maxWidth:proxy.size.width,alignment: .leading)
+                        
+                        VStack(spacing: 2) {
+                        
+                            Text("기록")
+                                .font(.efDiary(30))
+                                .foregroundColor(.memoPrimary)
+                            Text("(-)")
+                                .font(.efDiary(20))
+                                .foregroundColor(.memoPrimary)
+                                .frame(maxWidth:proxy.size.width)
+                                .overlay {
+                                    HStack{
+                                        //MARK: 보는 방법 버튼
+                                        Button {
+                                                viewModel.isDropDown.toggle()
+                                            
+                                        } label: {
+                                            HStack(spacing: 0){
+                                                Text(viewModel.wayToShow)
                                                     .font(.kotra(20))
-                                                    .foregroundColor(.memoDropDownText)
-                                                    .contentShape(Rectangle())
-                                                
-                                                Rectangle().frame(height: 1).padding(.horizontal,5)
-                                                    .foregroundColor(.memoDropDownText)
-                                                Text("작게보기")
-                                                    .font(.kotra (20))
-                                                    .foregroundColor(.memoDropDownText)
-                                                    .contentShape(Rectangle())
+                                                    .foregroundColor(.memoPrimary)
+                                                Image(systemName: "arrowtriangle.down.fill").foregroundColor(.memoPrimary)
                                             }
-                                            .padding(.horizontal,5)
-                                            .background(Color.memoDropDownBg)
-                                            .offset(y:25)
+                                        }
+                                        .overlay(alignment:.topLeading) {
+                                            //MARK: 드랍다운
+                                            if viewModel.isDropDown{
+                                                VStack(spacing:0){
+                                                    
+                                                    Text("크게보기")
+                                                        .font(.kotra(20))
+                                                        .foregroundColor(.memoDropDownText)
+                                                        .contentShape(Rectangle())
+                                                        .onTapGesture {
+                                                            
+                                                            viewModel.isDropDown.toggle()
+                                                            if(viewModel.wayToShow != "크게보기")
+                                                            {
+                                                                viewModel.wayToShow = "크게보기"
+                                                            }
+                                                                
+                                                            
+                                                        }
+                                                    
+                                                    Rectangle().frame(height: 1).padding(.horizontal,5)
+                                                        .foregroundColor(.memoDropDownText)
+                                                    Text("작게보기")
+                                                        .font(.kotra (20))
+                                                        .foregroundColor(.memoDropDownText)
+                                                        .contentShape(Rectangle())
+                                                        .onTapGesture {
+                                                            
+                                                            viewModel.isDropDown.toggle()
+                                                            if(viewModel.wayToShow != "작게보기")
+                                                            {
+                                                                viewModel.wayToShow = "작게보기"
+                                                            }
+                                                                
+                                                            
+                                                        }
+                                                }
+                                                .padding(.horizontal,5)
+                                                .background(Color.memoDropDownBg)
+                                                .offset(y:25)
+                                                
+                                            }
                                             
                                         }
-                                        
-                                    }
-                                    .zIndex(2.0)
-                                    .offset(x:thinPadding*2)
-                                
+                                        .zIndex(2.0)
+                                        .offset(x:thinPadding*2)
                                     
-                                    Spacer()
-                                    
-                                    Button {
                                         
-                                    } label: {
-                                        Image("memoCalendar")
-                                    }
-                                  
-                                    Button {
+                                        Spacer()
                                         
-                                    } label: {
-                                        Image("memoTrash")
-                                    }.padding(.trailing,thinPadding*2)
+                                        //MARK: 달력버튼
+                                        Button {
+                                            viewModel.showDatePicker.toggle()
+                                        } label: {
+                                            Image("memoCalendar")
+                                        }
+                                      
+                                        //MARK: 삭제버튼
+                                        Button {
+                                            viewModel.deleteMode.toggle()
+                                        } label: {
+                                            Image("memoTrash")
+                                        }.padding(.trailing,thinPadding*2)
 
+                                    }
                                 }
-                            }
-                            .zIndex(2)
+                                .zIndex(2)
+                            
+                            //MARK: 하단 사각형
+                            Rectangle()
+                                .fill(Color.memoPrimary)
+                                .frame(maxWidth:proxy.size.width,maxHeight: 10)
+                                .padding(.bottom,thinPadding).zIndex(1)
+                            
+                            Rectangle()
+                                .fill(Color.memoPrimary)
+                                .frame(maxWidth:proxy.size.width,maxHeight: 1)
+
+                        }
+                        .frame(maxWidth:proxy.size.width,maxHeight:proxy.size.height,alignment: .bottom)
                         
-                        Rectangle()
-                            .fill(Color.memoPrimary)
-                            .frame(maxWidth:proxy.size.width,maxHeight: 10)
-                            .padding(.bottom,thinPadding).zIndex(1)
+                        
+                        //MARK: 오른 사각형
+                        VStack{
+                            Rectangle()
+                                .fill(Color.memoPrimary)
+                                .frame(maxWidth:thin,maxHeight: proxy.size.height)
+                                .padding(.trailing,thinPadding)
+                        }.frame(maxWidth:proxy.size.width,alignment: .trailing)
                     }
-                    .frame(maxWidth:proxy.size.width,maxHeight:proxy.size.height,alignment: .bottom)
                     
-                    
-                    //오른쪽 사각형
-                    VStack{
-                        Rectangle()
-                            .fill(Color.memoPrimary)
-                            .frame(maxWidth:thin,maxHeight: proxy.size.height)
-                            .padding(.trailing,thinPadding)
-                    }.frame(maxWidth:proxy.size.width,alignment: .trailing)
                 }
+                .frame(maxHeight:ScreenSize.height/6,alignment: .bottom)
+                .background( Color.memoBg)
+                .zIndex(2)
+                //Header
                 
-            }
-            .frame(maxHeight:ScreenSize.height/6,alignment: .bottom)
-            .edgesIgnoringSafeArea(.top)
-            
-            
-            ScrollView()
-            {
                 
-            }
+                ScrollView(.vertical)
+                {
+                    LazyVGrid(columns: columns) {
+                        ForEach(memos){ memo in
+                            StickerView(memo: memo)
+                        }
+                    }
+                }
+                .padding(.vertical)
+                .background(Color.init(hexcode: "B2BFAC"))
+                
+                Image("memoBottomBanner")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth:.infinity,maxHeight: ScreenSize.height/6)
+                    
+                
+            } //Header
+            
         }
+        .edgesIgnoringSafeArea(.vertical)
     }
 }
 
---
+++ RaniPaper/Source/View/MemoView/StickerView.swift
@@ -0,0 +1,34 @@
+//
+//  StickerView.swift
+//  RaniPaper
+//
+//  Created by yongbeomkwak on 2022/12/01.
+//
+
+import SwiftUI
+
+struct StickerView: View {
+    
+    var memo:MemoModel
+    
+    var body: some View {
+        Image("sticker")
+            .overlay() {
+                VStack{
+                    Text(memo.title)
+                        .font(.title2)
+                    Divider()
+                    Text(memo.content).lineLimit(2).frame(maxWidth:.infinity,alignment: .leading)
+                        .font(.callout)
+                }
+                .padding()
+                .frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .topLeading)
+            }
+    }
+}
+
+struct StickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        StickerView(memo: MemoModel(title: "1234", content: "125521512"))
+    }
+}
