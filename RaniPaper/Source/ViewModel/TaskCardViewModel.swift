99681554ed912c879a4c58bd6bf8c2c20005d725
youn9k <ace_lephant@naver.com>
2022-12-01 15:50:58 +0900

    :sparkles: TaskCardViewModel 추가

---
+++ RaniPaper/Source/ViewModel/TaskCardViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  TaskCardViewModel.swift
+//  RaniPaper
+//
+//  Created by YoungK on 2022/12/01.
+//
+
+import Foundation
+
+final class TaskCardViewModel: ObservableObject{
+    
+    init() {
+        print("✅ TaskCardViewModel 생성")
+    }
+    
+    deinit {
+        print("❌ TaskCardViewModel 소멸")
+    }
+}
