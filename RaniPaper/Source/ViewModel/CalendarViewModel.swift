cbdd29441070ac08935b705522c19dc61e8d7008
youn9k <ace_lephant@naver.com>
2022-12-01 18:10:15 +0900

    :sparkles: CalendarViewModel init시 로컬에 저장된 내용 불러오기

--- RaniPaper/Source/ViewModel/CalendarViewModel.swift
+++ RaniPaper/Source/ViewModel/CalendarViewModel.swift
@@ -10,14 +10,30 @@ import SwiftUI
 final class CalendarViewModel: ObservableObject{
     
     @Published var currentDate: Date = Date()
-    @Published var showEdit:Bool = false
+    @Published var showEdit: Bool = false
+    @Published var tasks: [TaskModel] = []
+    
     init(){
         print("✅ CalendarViewModel 생성")
+        
+        fetchTasks()
+        
     }
     
     deinit{
         print("❌ CalendarViewModel 소멸")
     }
+    
+    func fetchTasks() {
+        let result = MyFileManager.shared.contentsOfDirectory(at: .diary)
+        switch result {
+        case .success(let data):
+            tasks = data.map { $0 as! TaskModel }.sorted(by: { $0.deadLine < $1.deadLine })
+        case .failure(let error):
+            print(error.errorDescription)
+        }
+    }
+    
 }
 
 
