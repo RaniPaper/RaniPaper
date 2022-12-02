fc1efff2ffd40b65a43045c5246327fa72b42d47
youn9k <ace_lephant@naver.com>
2022-12-02 22:31:00 +0900

    [CHORE] 임시용 롤링페이퍼 Array 추가

--- RaniPaper/Source/ViewModel/HomeViewModel.swift
+++ RaniPaper/Source/ViewModel/HomeViewModel.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 class HomeViewModel: ObservableObject {
- 
+    var rollingPapers: [String] = ["rolling_paper_temp_1", "rolling_paper_temp_2", "rolling_paper_temp_3"]
+    
     init() {
         print("✅ HomeViewModel 생성")
     }
