fdad170c1f603bca9959ed094acb4760958d9280
youn9k <ace_lephant@naver.com>
2022-12-08 17:56:51 +0900

    :lipstick: 갤러리 뷰 이동 버튼 추가

---
+++ RaniPaper/Source/View/GalleryView/GalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryView.swift
+//  RaniPaper
+//
+//  Created by YoungK on 2022/12/08.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
