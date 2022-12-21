20317dec746bf666f7b27d15243288a8c0fb0c39
yongbeomkwak <yongbeomkwak@gmail.com>
2022-12-21 20:16:11 +0900

    :lipstick: 인포 뷰 베이스 작업 완료

--- RaniPaper/Source/View/InfoView/InfoView.swift
+++ RaniPaper/Source/View/InfoView/InfoView.swift
@@ -10,9 +10,58 @@
 import SwiftUI
 
 struct InfoView: View {
+    
+    let height = ScreenSize.height/6
+    
     var body: some View {
-        VStack{
-            Text("'라니 페이퍼'입니다")
+        ZStack{
+            Color(hexcode: "B2BFAC")
+            // MARK: Header
+            VStack(spacing: 0) {
+                VStack(spacing: 0){
+                    Spacer()
+                    Text("앱 정보")
+                        .font(.efDiary(35))
+                        .foregroundColor(.memoPrimary)
+                    Text("App Information")
+                        .font(.efDiary(13))
+                        .foregroundColor(.memoPrimary)
+                    
+                    Spacer()
+                    Rectangle().frame(maxHeight: 2).foregroundColor(.memoPrimary)
+                }
+                .padding(.top,20)
+                .frame(maxWidth: .infinity,maxHeight: height)
+                .background {
+                    Color.memoBg
+            }
+                Spacer()
+                
+                VStack{
+                    
+                }.frame(maxWidth: .infinity,maxHeight: .infinity)
+                    
+                
+                
+                
+                VStack(spacing:20){
+                    Rectangle()
+                        .fill(Color.memoPrimary)
+                        .frame(maxWidth:.infinity,maxHeight: 1)
+                    Rectangle()
+                        .fill(Color(hexcode: "F4D4B6"))
+                        .frame(maxWidth:ScreenSize.width/3,maxHeight: 2)
+                   
+                    
+                        
+                    
+                    Spacer()
+                }.frame(maxWidth:.infinity,maxHeight: ScreenSize.height/6,alignment: .top)
+                    .background(Color.memoBg)
+            }
+           
+            
+            
         }
         .ignoresSafeArea()
     }
