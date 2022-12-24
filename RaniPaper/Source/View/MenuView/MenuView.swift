d406a46ead3f143a5fb87eaeabcacff0f6446b4c
nadongsuk@gmail.com <nadongsuk@gmail.com>
2022-12-24 17:23:07 +0900

    :bug: 스크롤 뷰와 사이드 메뉴 드래그 제스쳐 사이 충돌 해결

--- RaniPaper/Source/View/MenuView/MenuView.swift
+++ RaniPaper/Source/View/MenuView/MenuView.swift
@@ -14,75 +14,30 @@ struct MenuView: View {
     @GestureState var dragState: (CGFloat, Bool) = (Menu.minOffset, false)
     @State var isPlayed = false
     var body: some View {
-                
-        let drag = DragGesture(minimumDistance: 20)
-            .updating($dragState){(value, state, transcation) in
-                state.1 = true
-                if viewModel.isOpen{
-                    let cmp = Menu.maxOffset - value.startLocation.x
-                    if (value.translation.width > 0) && (cmp > 0) {
-                        state.0 = Menu.maxOffset + value.translation.width - cmp
-                    }
-                    else if (value.translation.width > 0) && (cmp < 0) {
-                        state.0 = Menu.maxOffset + value.translation.width
-                    }
-                } else {
-                    if value.startLocation.x > Menu.openEdge{
-                        state.0 = Menu.minOffset + value.translation.width > Menu.maxOffset ? Menu.minOffset + value.translation.width : Menu.maxOffset
-                    }
-                }
-            }
-            .onChanged{
-                if !viewModel.isOpen{
-                    if $0.startLocation.x > Menu.openEdge && !isPlayed{
-                        MySoundSetting.openSideMenu.play()
-                        isPlayed = true
-                    }
-                }
-                viewModel.Offset = dragState.0
-            }
-            .onEnded{ _ in
-                if viewModel.Offset < (viewModel.isOpen ? Menu.closeThreshold : Menu.openThreshold){
-                    withAnimation{
-                        viewModel.Offset = Menu.maxOffset
-                        viewModel.isOpen = true
-                    }
-                }
-                else {
-                    if viewModel.isOpen{
-                        MySoundSetting.closeSideMenu.play()
-                    }
-                    withAnimation{
-                        viewModel.isOpen = false
-                        viewModel.Offset = Menu.minOffset
-                    }
-                }
-                isPlayed = false
-            }
         
         ZStack{
             Color.black
-                .opacity(viewModel.Offset == Menu.minOffset ? 0 : Double((ScreenSize.width-viewModel.Offset))/1300)
+                .opacity(viewModel.Offset == Menu.minOffset ? 0 : Double((ScreenSize.width-viewModel.Offset))/1300+0.05)
                 .animation(.easeIn, value: viewModel.Offset)
-                .onTapGesture {
+                .gesture(TapGesture().onEnded{
                     if viewModel.isOpen{
                         MySoundSetting.closeSideMenu.play()
                         viewModel.Offset = Menu.minOffset
                         viewModel.isOpen = false
                     }
-                }
+                })
             
             SideMenuView(isOpen: $viewModel.isOpen,
                          offset: $viewModel.Offset,
                          selection: $selection,
                          viewModel: viewModel)
             .shadow(radius: 3)
-            .offset(x: dragState.1 ? dragState.0 : viewModel.Offset)
+            .offset(x: viewModel.Offset)
             .transition(.move(edge: .trailing))
             .animation(.linear, value: viewModel.Offset)
         }
         .contentShape(Rectangle())
-        .highPriorityGesture(userState.isMenuEnable ? drag : nil)
+        .modifier(DragGestureViewModifier(offset: $viewModel.Offset, isOpen: $viewModel.isOpen))
         .ignoresSafeArea()
     }
 }
@@ -100,5 +55,100 @@ struct MenuView_Previews: PreviewProvider {
         VStack{
             prev()
         }
+    }
+}
+
+struct DragGestureViewModifier: ViewModifier{
+    @GestureState private var isDragging: Bool = false
+    @State var gestureState: GestureStatus = .idle
+    @State var isPlayed = false
+    @Binding var offset: CGFloat
+    @Binding var isOpen: Bool
+    
+    var onUpdate: ((DragGesture.Value) -> Void)?
+    
+    func body(content: Content) -> some View {
+        content
+            .gesture(
+                DragGesture()
+                    .updating($isDragging){_, isDragging, _ in
+                        isDragging = true
+                    }
+                    .onChanged(onDragChange(_:))
+                    .onEnded(onDragEnded(_:))
+            )
+            .onChange(of: gestureState){ state in
+                guard state == .started else {return}
+                gestureState = .active
+                
+            }
+            .onChange(of: isDragging){ value in
+                if value, gestureState != .started{
+                    gestureState = .started
+                } else if !value, gestureState != .ended{
+                    gestureState = .cancelled
+                    onCancled()
+                }
+            }
+    }
+        
+    func onDragChange(_ value: DragGesture.Value){
+        guard gestureState == .started || gestureState == .active else {return}
+        onUpdate(value: value)
+    }
+        
+    func onDragEnded(_ value: DragGesture.Value){
+        gestureState = .ended
+        onEnd()
+    }
+    
+    func onUpdate(value: DragGesture.Value){
+        if isOpen{
+            let cmp = Menu.maxOffset - value.startLocation.x
+            if (value.translation.width > 0) && (cmp > 0) {
+                offset = max(Menu.maxOffset + value.translation.width - cmp, Menu.maxOffset)
+            }
+            else if (value.translation.width > 0) && (cmp < 0) {
+                offset = max(Menu.maxOffset + value.translation.width, Menu.maxOffset)
+            }
+            else{
+                offset = Menu.maxOffset
+            }
+        } else {
+            if value.startLocation.x > Menu.openEdge{
+                offset = max(Menu.minOffset + value.translation.width, Menu.maxOffset)
+                if !isPlayed{
+                    MySoundSetting.openSideMenu.play()
+                    isPlayed = true
+                }
+            }
+        }
+    }
+    
+    func onEnd(){
+        if offset < (isOpen ? Menu.closeThreshold : Menu.openThreshold){
+            withAnimation{
+                offset = Menu.maxOffset
+                isOpen = true
+            }
+        }
+        else {
+            if isOpen{
+                MySoundSetting.closeSideMenu.play()
+            }
+            withAnimation{
+                isOpen = false
+                offset = Menu.minOffset
+            }
+        }
+        isPlayed = false
+    }
+    
+    func onCancled(){
+        onEnd()
+    }
+    
+    enum GestureStatus: Equatable{
+        case idle, started, active, ended, cancelled
     }
 }
