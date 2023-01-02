7c0022f70da3e36661ffbf465077c37f76beeadd
nadongsuk@gmail.com <nadongsuk@gmail.com>
2023-01-02 13:05:37 +0900

    [CHORE] MainViewModel에 AVAudioSession 설정 추가

--- RaniPaper/Source/ViewModel/MainViewModel.swift
+++ RaniPaper/Source/ViewModel/MainViewModel.swift
@@ -8,20 +8,39 @@
 import Foundation
 import Combine
 import SwiftUI
+import AVKit
 
 final class MainViewModel : ObservableObject {
     @Published var isLoading: Bool
     @Published var selection: ViewSelection
+    let audioSession = AVAudioSession.sharedInstance()
 
     init(){
         self.isLoading = true
         self.selection = .home
+        do {
+            try audioSession.setCategory(.playback, mode: .default, options: [.mixWithOthers])
+        } catch {
+           print("🔥 오디오 세션 설정에 실패했습니다")
+        }
+        
+        do {
+            try audioSession.setActive(true)
+        } catch {
+            print("🔥 오디오 세션 활성에 실패했습니다")
+        }
         
         print("✅ MainViewModel 생성")
     }
     
     deinit{
         print("❌ MainViewModel 소멸")
+        
+        do {
+            try audioSession.setActive(false)
+        } catch {
+            print("🔥 오디오 세션 비활성에 실패했습니다")
+        }
     }
     
 }
